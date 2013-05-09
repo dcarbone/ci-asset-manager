4b0fc29ea5eacd3eb296931d7093a06b3fd82bf6
Daniel Carbone <daniel.p.carbone@gmail.com>
2013-05-09 13:46:29 -0500

    Updating license.

--- libraries/DCarbone/AssetManager/Generics/Complex.php
+++ libraries/DCarbone/AssetManager/Generics/Complex.php
@@ -14,13 +14,24 @@ use \DCarbone\AssetManager\Specials\Assets\Script;
 use \DCarbone\AssetManager\Specials\Assets\Style;
 use \DCarbone\AssetManager\Specials\Assets\ScriptView;
 
-/**
- * Asset Packager Complex Asset class
- *
- * @version 1.0
- * @author Daniel Carbone (daniel.p.carbone@vanderbilt.edu)
- *
+/*
+    Complex Output Class for Asset Management Library
+    Copyright (C) 2013  Daniel Carbone (https://github.com/dcarbone)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 class Complex
 {
     protected static $CI = null;
