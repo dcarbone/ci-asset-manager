a8b8bab1fda4a210ff375ad9d1b6b4a0e932db91
Daniel Carbone <daniel.p.carbone@gmail.com>
2014-07-13 12:52:17 -0500

    Removing useless shortcuts.....

--- demo/application/core/MY_Controller.php
+++ demo/application/core/MY_Controller.php
@@ -1,10 +1,36 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Class MY_Controller
+ * Class \CI_Controller
  */
 class MY_Controller extends CI_Controller
 {
+    /** @var \CI_Input */
+    public $input;
+    /** @var \CI_Router */
+    public $router;
+    /** @var \CI_Config */
+    public $config;
+    /** @var \CI_Email */
+    public $email;
+    /** @var \CI_Output */
+    public $output;
+    /** @var \CI_Log */
+    public $log;
+
+    // Base core classes
+    /** @var CI_Session */
+    public $session;
+    /** @var CI_Loader */
+    public $load;
+    /** @var CI_User_Agent */
+    public $agent;
+    /** @var CI_URI */
+    public $uri;
+
+    /** @var \asset_manager */
+    public $asset_manager;
+    
     /**
      * Constructor
      */
@@ -12,6 +38,6 @@ class MY_Controller extends CI_Controller
     {
         parent::__construct();
 
-        $this->load->library('asset_manager');
+
     }
 }
