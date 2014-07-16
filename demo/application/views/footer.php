c5fbae4401810650e17322573f4b4f469299d94e
Daniel Carbone <daniel.p.carbone@gmail.com>
2014-07-16 12:02:40 -0500

    Removing demo app from codebase with switch to composer.

--- demo/application/views/footer.php
+++
@@ -1,6 +0,0 @@
-</div><!-- div#body -->
-
-<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-</div><!-- div#container -->
-</body>
-</html>
--- demo/application/views/welcome_message.php
+++
@@ -1,4 +0,0 @@
-<p>
-    Fill this out with super interesting things.
-</p>
-<code>&lt;?php echo $this->asset_manager->generate_output(); ?></code>
