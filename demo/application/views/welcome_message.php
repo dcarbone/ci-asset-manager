9d7d5533be30d976bbd41d29b962fd675787c75a
Daniel Carbone <daniel.p.carbone@gmail.com>
2014-10-02 13:22:25 -0500

    Updating demo app a little bit

--- demo/application/views/welcome_message.php
+++ demo/application/views/welcome_message.php
@@ -1,4 +1,9 @@
 <p>
     Fill this out with super interesting things.
 </p>
-<code>&lt;?php echo $this->asset_manager->generate_output(); ?></code>
+<code>&lt;?php<br>
+    echo $this->asset_manager->generate_asset_tag('js/jquery-1.11.1.min.js', true);
+    <br>
+    echo $this->asset_manager->generate_asset_tag('css/basic.css', true);
+    <br>
+?></code>
