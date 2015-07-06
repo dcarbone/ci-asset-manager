36e6c591a582f59821a373ab83a67c629c4b838f
Daniel Carbone <daniel.p.carbone@gmail.com>
2015-07-06 15:14:12 -0500

    Removing demo app.

--- demo/application/views/view.php
+++
@@ -1,7 +0,0 @@
-<p>
-    Fill this out with super interesting things.
-</p>
-<pre><code>&lt;?php
-    echo $this->asset_manager->generate_asset_tag('js/jquery-1.11.1.min.js', true);
-    echo $this->asset_manager->generate_asset_tag('css/basic.css', true);
-?></code></pre>
--- demo/assets/js/noty-woot.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-
-    var n = noty({
-        text: 'Noty was loaded!  Woot!',
-        layout: 'center',
-        type: 'success'
-    });
-
-});
